e1c7a8f021b4b51af9da5c1820d7afef9d19055a
Akshay Bavaliya <akshaybavaliya007@gmail.com>
2025-09-02 20:09:33 +0530

    authentication and subscription implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,31 @@
-import Image from "next/image"
-import Link from "next/link"
-import NavItems from "./NavItems"
+import Image from "next/image";
+import Link from "next/link";
+import NavItems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between mx-auto w-full px-14 py-4 bg-white max-sm:px-4">
-            <Link href="/">
-                <div className="flex items-center gap-2.5 cursor-pointer">
-                    <Image
-                        src="/images/logo.png"
-                        alt="logo"
-                        width={46}
-                        height={44}
-                    />
-                </div>
-            </Link>
-            <div className="flex items-center gap-8">
-                <NavItems />
-            </div>
-        </nav>
-  )
-}
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.png" alt="logo" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <SignedOut>
+          <SignInButton>
+            <button className="border border-black rounded-4xl px-4 py-2.5 text-sm font-semibold flex items-center gap-2 cursor-pointer">
+              Sign In
+            </button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
